b7f854f03ba7f9b42ed0f459310a34bef3c02ad8
Lakshmi Balasubramaniam <lrb697@gmail.com>
2022-03-15 15:24:20 -0400

    Added Review, Approve, Reject & GetReviews methods to RequestController

--- src/main/java/com/maxtrain/capstone/request/RequestController.java
+++ src/main/java/com/maxtrain/capstone/request/RequestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/requests")
 public class RequestController {
+	
 	@Autowired
 	private RequestRepository reqRepo;
 	
@@ -24,6 +25,14 @@ public class RequestController {
 			return new ResponseEntity<> (HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<Request>(req.get(),HttpStatus.OK);
+	}
+	
+	// GetReviews(userId) - Gets requests in "REVIEW" status and not owned by the user with the primary key of id.
+	// GET: /api/requests/review/{userId}
+	@GetMapping("review/{userId}")
+	public ResponseEntity<Iterable<Request>> getRequestsInReview(@PathVariable int userId) {
+		var requests = reqRepo.findByStatusAndUserId("REVIEW", userId);
+		return new ResponseEntity<Iterable<Request>>(requests, HttpStatus.OK);
 	}
 	
 	@PostMapping
@@ -49,6 +58,35 @@ public class RequestController {
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
+	// Review(request) - Sets the status of the request for the id provided to "REVIEW" unless the total
+	// of the request is less than or equal to $50. If so, it sets the status directly to "APPROVED"
+	// PUT: /api/requests/5/review
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}/review")
+	public ResponseEntity reviewRequest(@PathVariable int id, @RequestBody Request request) {
+		var status = (request.getTotal() <= 50) ? "APPROVED" : "REVIEW";
+		request.setStatus(status);
+		return putRequest(id, request);
+	}
+	
+	// Approve(request) - Sets the status of the request for the id provided to "APPROVED"
+	// PUT: /api/requests/5/approve
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}/approve")
+	public ResponseEntity approveRequest(@PathVariable int id, @RequestBody Request request) {
+		request.setStatus("APPROVED");
+		return putRequest(id, request);
+	}
+	
+	// Reject(request) - Sets the status of the request for the id provided to "REJECTED"
+	// PUT: /api/requests/5/reject
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}/reject")
+	public ResponseEntity rejectRequest(@PathVariable int id, @RequestBody Request request) {
+		request.setStatus("REJECTED");
+		return putRequest(id, request);
+	}
+	
 	@SuppressWarnings("rawtypes")
 	@DeleteMapping("{id}")
 	public ResponseEntity deleteRequest(@PathVariable int id) {
@@ -59,9 +97,13 @@ public class RequestController {
 		reqRepo.delete(req.get());
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
+	
 }
+
 // Notes:
 // CrossOrigin is an annotation that is like to useCords in Startup.cs in prs-server-C35 (the C# prs backend)
 // & CrossOrigin makes sure that the frontend can talk to the backend
 
 // RestController signifies that sends & receives JSON data
+
+// @SuppressWarnings("rawtypes") is there so that can use the ResponseEntity without the <> after the ResponseEntity
--- src/main/java/com/maxtrain/capstone/request/RequestRepository.java
+++ src/main/java/com/maxtrain/capstone/request/RequestRepository.java
@@ -3,5 +3,5 @@ package com.maxtrain.capstone.request;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RequestRepository extends CrudRepository<Request, Integer>{
-	
+	Iterable<Request> findByStatusAndUserId(String status, int userId);
 }
